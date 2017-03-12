351f589fd68584f8b98cc9fd0880c5b8ed06137a
Media Streaming Websites <support@s3bubble.com>
2017-03-12 22:56:34 +0000

    cloudfront tests

--- woocommerce/global/wrapper-start.php
+++ woocommerce/global/wrapper-start.php
@@ -20,28 +20,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-$template = get_option( 'template' );
-
-switch( $template ) {
-	case 'twentyeleven' :
-		echo '<div id="primary"><div id="content" role="main" class="twentyeleven">';
-		break;
-	case 'twentytwelve' :
-		echo '<div id="primary" class="site-content"><div id="content" role="main" class="twentytwelve">';
-		break;
-	case 'twentythirteen' :
-		echo '<div id="primary" class="site-content"><div id="content" role="main" class="entry-content twentythirteen">';
-		break;
-	case 'twentyfourteen' :
-		echo '<div id="primary" class="content-area"><div id="content" role="main" class="site-content twentyfourteen"><div class="tfwc">';
-		break;
-	case 'twentyfifteen' :
-		echo '<div id="primary" role="main" class="content-area twentyfifteen"><div id="main" class="site-main t15wc">';
-		break;
-	case 'twentysixteen' :
-		echo '<div id="primary" class="content-area twentysixteen"><main id="main" class="site-main" role="main">';
-		break;
-	default :
-		echo '<main class="cd-main-content"><section class="s3bubble-woocommerce"><div class="container"><div class="row">';
-		break;
-}
+echo '<main class="cd-main-content"><section class="s3bubble-woocommerce"><div class="container-fluid"><div class="row">';
