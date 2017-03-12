d3447396a54c8d744b11dc7d3e70ecb93a83007e
Media Streaming Websites <support@s3bubble.com>
2017-03-12 22:50:55 +0000

    cloudfront tests

--- dist/js/menu.js
+++ dist/js/menu.js
@@ -90,18 +90,18 @@ jQuery(document).ready(function($){
 	}
 
 	function toggleSearch(type) {
-		if(type=="close") {
+		if(type==="close") {
 			//close serach 
 			$('.cd-search').removeClass('is-visible');
 			$('.cd-search-trigger').removeClass('search-is-visible');
-			$('.cd-overlay').removeClass('search-is-visible');
+			$('.cd-overlay').removeClass('search-is-visible');	
 		} else {
 			//toggle search visibility
 			$('.cd-search').toggleClass('is-visible');
 			$('.cd-search-trigger').toggleClass('search-is-visible');
 			$('.cd-overlay').toggleClass('search-is-visible');
 			if($(window).width() > MqL && $('.cd-search').hasClass('is-visible')) $('.cd-search').find('input[type="search"]').focus();
-			($('.cd-search').hasClass('is-visible')) ? $('.cd-overlay').addClass('is-visible') : $('.cd-overlay').removeClass('is-visible') ;
+			($('.cd-search').hasClass('is-visible')) ? $('.cd-overlay').addClass('is-visible') : $('.cd-overlay').removeClass('is-visible');
 		}
 	}
 
