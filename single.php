519cb548c1e57697917a632b2aa25f4886839845
Media Streaming Websites <support@s3bubble.com>
2017-03-23 00:24:20 +0000

    woo fixes

--- single.php
+++ single.php
@@ -5,12 +5,6 @@
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
     	<div id="s3bubble-aws" class="video-wrapper">
-    		<?php
-		    	$s3videoid = get_post_meta( get_the_ID(), 's3bubble_video_code_meta_box_text', true );
-				// Check if the custom field has a value.
-				if ( ! empty( $s3videoid ) ) : ?>
-					<div style="position: relative;padding-bottom: 56.25%;"><iframe style="position: absolute;top: 0;left: 0;width: 100%;height: 100%;" src="<?php echo $s3videoid; ?>" frameborder="0" allowfullscreen="allowfullscreen"></iframe></div>
-			<?php endif; ?>
 		 	<?php //the_content(); ?>
 		</div>
 
@@ -28,11 +22,11 @@
 	   update_post_meta($post->ID,'recently_watched_user_id',get_current_user_id());
 	endif;
 
-	/*$s3videoid = get_post_meta( get_the_ID(), 's3bubble_video_code_meta_box_text', true );
+	$s3videoid = get_post_meta( get_the_ID(), 's3bubble_video_code_meta_box_text', true );
 	// Check if the custom field has a value.
 	if ( ! empty( $s3videoid ) ) {
 	    echo '<script type="text/javascript">S3BubbleAWS.init({id : "s3bubble-aws",code : "' . $s3videoid . '"});</script>';
-	}*/
+	}
 
 ?>
 
