1a593cda3fac6a07cb46723cee7340445d616a70
Media Streaming Websites <support@s3bubble.com>
2017-04-27 21:42:15 +0100

    woo.php fix

--- inc/theme/woocommerce.php
+++ inc/theme/woocommerce.php
@@ -109,7 +109,10 @@ if (class_exists('WooCommerce_Membership_Post')) {
 
                   // Filter out restricted post
                   $meta = get_post_meta( $post->ID );
-                  $post->premium = (!empty($meta['_rpwcm_post_restriction_method'][0] == "none") ? false : true );
+                  $post->premium = false;
+                  if(isset($meta['_rpwcm_post_restriction_method'][0])){
+                    $post->premium = true;
+                  }
                   $post->plans = (!empty($meta['_rpwcm_only_caps']) ? $meta['_rpwcm_only_caps'] : null);
 
               }
