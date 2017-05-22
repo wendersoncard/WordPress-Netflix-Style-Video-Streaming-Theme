c6e706355422fa9d215ffb5ce4db350494c53182
Media Streaming Websites <support@s3bubble.com>
2017-05-22 19:10:30 +0100

    switched to dev with dist folder

--- templates/content-single.php
+++
@@ -1,5 +0,0 @@
-<div class="video-main video-player-streaming">
-
-	<div id="s3bubble-<?php echo get_the_ID(); ?>" class="video-wrapper"></div>
-
-</div>
--- tests.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-	/*
-	 Template Name: Tests Page Template
-	 */
